7e9d65c00ced08d54bc9c38ff0a81a5e65054d21
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-21 02:51:01 +0000

    strip perf

--- device-shusky-common.mk
+++ device-shusky-common.mk
@@ -24,10 +24,5 @@ PRODUCT_COPY_FILES += \
 	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
 	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_charge_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_charge.json
 
-# Power HAL config
-ifeq (,$(TARGET_VENDOR_PERF_CONFIG_PATH))
-TARGET_VENDOR_PERF_CONFIG_PATH := device/google/shusky/perf
-endif
-
 PRODUCT_COPY_FILES += \
 	$(TARGET_VENDOR_PERF_CONFIG_PATH)/powerhint-$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
