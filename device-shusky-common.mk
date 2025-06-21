bd77e104d9635ff6f88d4c37fa43ccb394dc4d17
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-21 02:51:07 +0000

    strip thermal

--- device-shusky-common.mk
+++ device-shusky-common.mk
@@ -14,15 +14,5 @@
 # limitations under the License.
 #
 
-
-# Thermal Config
-ifeq (,$(TARGET_VENDOR_THERMAL_CONFIG_PATH))
-TARGET_VENDOR_THERMAL_CONFIG_PATH := device/google/shusky/thermal
-endif
-
-PRODUCT_COPY_FILES += \
-	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json \
-	$(TARGET_VENDOR_THERMAL_CONFIG_PATH)/thermal_info_config_charge_$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config_charge.json
-
 PRODUCT_COPY_FILES += \
 	$(TARGET_VENDOR_PERF_CONFIG_PATH)/powerhint-$(TARGET_DEVICE).json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
