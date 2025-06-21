c77168f8754cd70fa0a34b1bc5ce30f4c5242890
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-21 02:51:31 +0000

    strip uwb

--- uwb/uwb_calibration.mk
+++ uwb/uwb_calibration.mk
@@ -12,14 +12,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-PRODUCT_PACKAGES += \
-    configuration.conf \
-    calibration.conf \
-    calibration-default.conf \
-    calibration-CE.conf \
-    calibration-FCC.conf \
-    calibration-JP.conf \
-    calibration-TW.conf \
-    calibration-RESTRICTED.conf
-
