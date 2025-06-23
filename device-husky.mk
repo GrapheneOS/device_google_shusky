091f84fdcfc2eab61a09f908e941ef2913f57942
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 14:09:26 +0000

    strip bluetooth opus props

--- device-husky.mk
+++ device-husky.mk
@@ -122,10 +122,6 @@ endif
 PRODUCT_PACKAGES_DEBUG += \
     hci_inject
 
-# Bluetooth OPUS codec
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.opus.enabled=true
-
 # Bluetooth SAR test tool
 PRODUCT_PACKAGES_DEBUG += \
     sar_test
