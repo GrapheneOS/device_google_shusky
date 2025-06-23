fcfb14a7961b9c7b867ba0e2190eb9c9f26c8ce2
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 14:09:26 +0000

    strip bluetooth opus props

--- device-shiba.mk
+++ device-shiba.mk
@@ -109,10 +109,6 @@ endif
 PRODUCT_PACKAGES_DEBUG += \
     hci_inject
 
-# Bluetooth OPUS codec
-PRODUCT_PRODUCT_PROPERTIES += \
-    persist.bluetooth.opus.enabled=true
-
 # Bluetooth SAR test tool
 PRODUCT_PACKAGES_DEBUG += \
     sar_test
