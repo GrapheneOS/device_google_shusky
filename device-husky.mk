3b27f83b9fb2c7d56532a080d9d7591be9059a4d
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-23 14:22:22 +0000

    strip vibrator hal related props

--- device-husky.mk
+++ device-husky.mk
@@ -187,15 +187,6 @@ endif
 $(call soong_config_set,haptics,kernel_ver,v$(subst .,_,$(TARGET_LINUX_KERNEL_VERSION)))
 ACTUATOR_MODEL := luxshare_ict_081545
 ADAPTIVE_HAPTICS_FEATURE := adaptive_haptics_v1
-PRODUCT_VENDOR_PROPERTIES += \
-    persist.vendor.vibrator.hal.chirp.enabled=0 \
-    ro.vendor.vibrator.hal.device.mass=0.222 \
-    ro.vendor.vibrator.hal.loc.coeff=2.8 \
-    persist.vendor.vibrator.hal.context.enable=false \
-    persist.vendor.vibrator.hal.context.scale=60 \
-    persist.vendor.vibrator.hal.context.fade=true \
-    persist.vendor.vibrator.hal.context.cooldowntime=1600 \
-    persist.vendor.vibrator.hal.context.settlingtime=5000
 
 # Override Output Distortion Gain
 PRODUCT_VENDOR_PROPERTIES += \
